5224c3309ef24dc0445aecbd04748630a0cf8fd8
kqjiang <atlaskqjiang@gmail.com>
2017-08-09 21:16:05 -0400

    updated media.py and entertainment_center.py
    for 2nd code review
    added comments
    fixed style issues according to PEP-8 guideline

--- Project 1_Movie Trailer Website/entertainment_center.py
+++ Project 1_Movie Trailer Website/entertainment_center.py
@@ -2,43 +2,60 @@ import fresh_tomatoes
 import media
 
 # below is a list of movies available
-# movie instances with detailed information are created by meida.Movie() function
-toy_story = media.Movie("Toy Story",
-                        "A story of a boy and his toy come to life",
-                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+# movie instances with detailed information are created by meida.Movie()
+toy_story = media.Movie(
+	"Toy Story",
+    "A story of a boy and his toy come to life",
+    "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=KYz2wyBy3kc"
+    )
 
 
-avatar = media.Movie("Avatar",
-                     "A marine on an alien plant",
-                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
-                     "https://www.youtube.com/watch?v=-9ceBgWV8io")
+avatar = media.Movie(
+    "Avatar",
+    "A marine on an alien plant",
+    "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=-9ceBgWV8io"
+    )
 
 
-school_of_rock = media.Movie("School of Rock",
-                             "Storyline",
-                             "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
-                             "https://www.youtube.com/watch?v=3PsUJFEBC74")
+school_of_rock = media.Movie(
+    "School of Rock",
+    "Storyline",
+    "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=3PsUJFEBC74"
+    )
 
-ratatouille = media.Movie("Ratatouille",
-                          "Storyline",
-                          "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
-                          "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+ratatouille = media.Movie(
+    "Ratatouille",
+    "Storyline",
+    "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=c3sBBRxDAqk"
+    )
 
-midnight_in_paris = media.Movie("Midnight in Paris",
-                                "Storyline",
-                                "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
-                                "https://www.youtube.com/watch?v=FAfR8omt-CY")
+midnight_in_paris = media.Movie(
+    "Midnight in Paris",
+    "Storyline",
+    "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=FAfR8omt-CY"
+    )
 
-hunger_games = media.Movie("Hunger Games",
-                           "Storyline",
-                           "http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
-                           "https://www.youtube.com/watch?v=4S9a5V9ODuY")
+hunger_games = media.Movie(
+    "Hunger Games",
+    "Storyline",
+    "http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=4S9a5V9ODuY"
+    )
 
-# create an array for these movies above
-movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
+# create an array set for these movies above that will pass to the media file
+movies = [
+    toy_story,
+    avatar,
+    school_of_rock,
+    ratatouille,
+    midnight_in_paris,
+    hunger_games
+    ]
 
-# create and open the website using fresh_tomatoes.open_movies_page()
+# create and open the html website using fresh_tomatoes.open_movies_page()
 fresh_tomatoes.open_movies_page(movies)
-
-
--- Project 1_Movie Trailer Website/media.py
+++ Project 1_Movie Trailer Website/media.py
@@ -1,18 +1,15 @@
 import webbrowser
 
-class Movie():
-    """"This class provides a way to store movie related information"""
-    
-    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-    
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
 
+# Movie class contains detail information and the show_trailer operation
+class Movie():
+    # this is the constructor of the Movie class with inputs and outputs
+    def __init__(self, movie_title, storyline, poster_image, trailer_url):
+        self.title = movie_title
+        self.storyline = storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_url
+
+    # this is a function to open the movie trailer url on youtube by webbrowser
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-        
-        
-        
